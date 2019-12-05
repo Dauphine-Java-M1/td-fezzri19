fb7dc20b32e14d2d9fe306f1d7b6ef2ea45cc119
fezzoua <riad.fezzoua@dauphine.eu>
2019-12-05 23:33:27 +0100

    commit 1

--- td00/src/main/java/fr/dauphine/ja/fezzouariad/td00/PrimeCollection.java
+++ td00/src/main/java/fr/dauphine/ja/fezzouariad/td00/PrimeCollection.java
@@ -9,46 +9,49 @@ import java.util.Random;
  */
 public class PrimeCollection 
 {
+	// Rajout commentaire pour test commit
+	
 	private ArrayList<Integer> numbers;
 	private Random random = new Random();
-    public PrimeCollection(ArrayList<Integer> n) {
-		this.numbers=n;
+	public PrimeCollection() 
+	{
+		this.numbers = new ArrayList<>();
 	}
-    public void initrandom(int n,int m) {
-    	for (int i = 0; i < n; i++) {
-			int init=random.nextInt(m);
-			numbers.add(init);
+	
+	public void initRandon(int n,int m)
+	{
+		for(int i = 0 ; i<=n; i++) {
+			int rajout = random.nextInt(m);
+			numbers.add(rajout);
 		}
-    }
-    public boolean isPrime(int p) {
-    	if (p<=1)  return false;
-    	for (int i = 2; i < Math.sqrt(p); i++) {
-			if(p%i==0) return false;
+	}
+	/**
+	 *  Modification de la methode isPrime() pour pouvoir y accéder.
+	 * @param p
+	 * @return
+	 */
+	public boolean isPrime(int p) 
+	{
+		if(p<1) return false;
+		for(int i =2; i<Math.sqrt(p); i++) {
+			if(p%i == 0) return false;
 		}
-    	return true;
-    	
-		
-    }
-    public void printPrimes() {
-    	for(int e : numbers) {
-    		if(isPrime(e)) {
-    			System.out.println(e);
-    		}
-    	}
-    }
-    	
-    
-	public static void main( String[] args )
+		return true;
+	}
+	
+	public void printPrimes() 
+	{
+		for(Integer e : this.numbers) {
+			if(isPrime(e)) {
+				System.out.println(e);
+			}
+		}
+	}
+	
+    public static void main( String[] args )
     {
-        ArrayList<Integer> n = new ArrayList<>();
-        PrimeCollection p=new PrimeCollection(n);
-        p.initrandom(100, 2000);
-        System.out.println(n);
-        p.printPrimes();
-        
-        
-        
-        
-        
+       PrimeCollection primeCollection = new PrimeCollection();
+       primeCollection.initRandon(100, 2000);
+       primeCollection.printPrimes();
     }
 }
